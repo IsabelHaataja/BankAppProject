c42af74ec3711289ab83e625f35f3d56b415567f
Isabel Haataja <isabelhaataja@gmail.com>
2024-04-24 11:58:14 +0200

    Added Country enum class and CountryExtension class for country code

---
+++ DataAccessLayer/Models/CountrySystem/CountryExtension.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Models.CountrySystem
+{
+    public static class CountryExtension
+    {
+        public static string GetCountryCode(this Country country)
+        {
+            switch (country)
+            {
+                case Country.Sweden:
+                    return "SE";
+                case Country.Finland:
+                    return "FI";
+                case Country.Denmark:
+                    return "DK";
+                case Country.Norway:
+                    return "NO";
+                default:
+                    throw new ArgumentException("Country code not found for the specified country.", nameof(country));
+            }
+        }
+        //How to use:
+        //using CountrySystem;
+        //Country country = Country.Sweden;
+        //string countryCode = country.GetCountryCode();
+    }
+}
