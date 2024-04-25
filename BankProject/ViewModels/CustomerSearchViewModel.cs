5d648d9d3e55ecbdb836e4cabf7047a8494dde91
Isabel Haataja <isabelhaataja@gmail.com>
2024-04-25 14:54:12 +0200

    Created ViewModel for Customer search

---
+++ BankProject/ViewModels/CustomerSearchViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace BankProject.ViewModels
+{
+    public class CustomerSearchViewModel
+    {
+        public int CustomerId { get; set; }
+
+        public string Givenname { get; set; } = null!;
+
+        public string Surname { get; set; } = null!;
+
+        public string Streetaddress { get; set; } = null!;
+
+        public string City { get; set; } = null!;
+
+        public string? NationalId { get; set; }
+        public List<CustomerSearchViewModel> Customers { get; set; }
+    }
+}
