ccebe37808210e4e4a0a87041c41708a0cb4153e
Isabel Haataja <isabelhaataja@gmail.com>
2024-05-28 15:00:44 +0200

    Made CustomerNumber and AccountNumber Non nullable

--- DataAccessLayer/Models/DataInitializer.cs
+++ DataAccessLayer/Models/DataInitializer.cs
@@ -26,14 +26,12 @@ namespace DataAccessLayer.Models
 			SeedUsers();
 		}
 
-		// Här finns möjlighet att uppdatera dina användares loginuppgifter
 		private void SeedUsers()
 		{
 			AddUserIfNotExists("richard.chalk@systementor.se", "Hejsan123#", new string[] { "Admin" });
 			AddUserIfNotExists("richard.chalk@customer.systementor.se", "Hejsan123#", new string[] { "Cashier" });
 		}
 
-		// Här finns möjlighet att uppdatera dina användares roller
 		private void SeedRoles()
 		{
 			AddRoleIfNotExisting("Admin");
