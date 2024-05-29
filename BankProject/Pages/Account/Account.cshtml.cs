7e2f30c21f6a35e0540f5f9e0ef4ae33f345b001
Isabel Haataja <isabelhaataja@gmail.com>
2024-05-29 16:06:17 +0200

    Removed the first Bank Api that was not working

---
+++ BankProject/Pages/Account/Account.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BankProject.Pages.Account
+{
+    public class AccountModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
