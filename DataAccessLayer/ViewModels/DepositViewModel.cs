cd915d075aff808153601a35b9b4f2a1a28ea092
Isabel Haataja <isabelhaataja@gmail.com>
2024-05-30 15:48:25 +0200

    Created partial views and view models for Deposit and withdrawal

---
+++ DataAccessLayer/ViewModels/DepositViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.ViewModels
+{
+    public class DepositViewModel
+    {
+        public int AccountId { get; set; }
+        public decimal Amount { get; set; }
+        public string Comment { get; set; }
+    }
+}
