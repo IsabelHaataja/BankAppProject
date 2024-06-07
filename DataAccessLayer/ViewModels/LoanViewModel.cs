635d50745199960d725264bf535e1a8614f5f6d0
Isabel Haataja <isabelhaataja@gmail.com>
2024-06-07 16:13:10 +0200

    Created ViewModels for Loans and permanent orders

---
+++ DataAccessLayer/ViewModels/LoanViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.ViewModels
+{
+    public class LoanViewModel
+    {
+        public int LoanId { get; set; }
+        public DateOnly Date { get; set; }
+        public decimal Amount { get; set; }
+        public int Duration { get; set; }
+        public decimal Payments { get; set; }
+        public string Status { get; set; } = null!;
+    }
+}
---
+++ DataAccessLayer/ViewModels/PermanentOrderViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.ViewModels
+{
+    public class PermanentOrderViewModel
+    {
+        public int OrderId { get; set; }
+        public string BankTo { get; set; } = null!;
+        public string AccountTo { get; set; } = null!;
+        public decimal? Amount { get; set; }
+        public string Symbol { get; set; } = null!;
+    }
+}
