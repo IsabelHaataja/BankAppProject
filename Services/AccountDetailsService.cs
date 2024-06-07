76a554ec43dd078aa833b1aa3c10f51cf2b313b0
Isabel Haataja <isabelhaataja@gmail.com>
2024-06-07 16:12:13 +0200

    Created service for Loans and permanent orders for AccountDetails page

---
+++ Services/AccountDetailsService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using DataAccessLayer;
+using DataAccessLayer.Models;
+using DataAccessLayer.ViewModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace Services
+{
+    public class AccountDetailsService : IAccountDetailsService
+    {
+        private readonly DataAccessService _dataAccessService;
+        private readonly IMapper _mapper;
+        public AccountDetailsService (DataAccessService dataAccessService, IMapper mapper)
+        {
+            _dataAccessService = dataAccessService;
+            _mapper = mapper;
+        }
+
+        public async Task<List<LoanViewModel>> GetLoansByAccountIdAsync(int accountId)
+        {
+            return await _dataAccessService.GetDbContext().Loans
+                .Where(l => l.AccountId == accountId)
+                .Select(l => new LoanViewModel
+                {
+                    LoanId = l.LoanId,
+                    Date = l.Date,
+                    Amount = l.Amount,
+                    Duration = l.Duration,
+                    Payments = l.Payments,
+                    Status = l.Status
+                })
+                .ToListAsync();
+        }
+
+        public async Task<List<PermanentOrderViewModel>> GetPermanentOrdersByAccountIdAsync(int accountId)
+        {
+            return await _dataAccessService.GetDbContext().PermenentOrders
+                .Where(po => po.AccountId == accountId)
+                .Select(po => new PermanentOrderViewModel
+                {
+                    OrderId = po.OrderId,
+                    BankTo = po.BankTo,
+                    AccountTo = po.AccountTo,
+                    Amount = po.Amount,
+                    Symbol = po.Symbol
+                })
+                .ToListAsync();
+        }
+    }
+}
---
+++ Services/IAccountDetailsService.cs
@@ -0,0 +1,15 @@
+﻿using DataAccessLayer.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public interface IAccountDetailsService
+    {
+         Task<List<PermanentOrderViewModel>> GetPermanentOrdersByAccountIdAsync(int accountId);
+         Task<List<LoanViewModel>> GetLoansByAccountIdAsync(int accountId);
+    }
+}
