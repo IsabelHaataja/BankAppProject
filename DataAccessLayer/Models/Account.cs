d028a046397b8604feb2daf44ac71dc51155a949
Isabel Haataja <isabelhaataja@gmail.com>
2024-06-12 12:46:07 +0200

    Migrated nullable fields to database.

--- DataAccessLayer/Models/Account.cs
+++ DataAccessLayer/Models/Account.cs
@@ -7,7 +7,7 @@ public partial class Account
 {
     public int AccountId { get; set; }
 
-	public string AccountNumber { get; set; }
+	public string? AccountNumber { get; set; }
 
 	public string Frequency { get; set; } = null!;
 
