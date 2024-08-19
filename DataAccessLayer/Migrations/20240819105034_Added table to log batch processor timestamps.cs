5a664b9bc8b6fe853d0c7e451034420a0d1dd0df
Isabel Haataja <isabelhaataja@gmail.com>
2024-08-19 13:03:44 +0200

    Added a class for tracking when the transactions were last processed.

---
+++ DataAccessLayer/Migrations/20240819105034_Added table to log batch processor timestamps.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class Addedtabletologbatchprocessortimestamps : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "LastProcesseds",
+                columns: table => new
+                {
+                    LastProcessedId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CustomerId = table.Column<int>(type: "int", nullable: false),
+                    LastProcessedDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LastProcesseds", x => x.LastProcessedId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LastProcesseds");
+        }
+    }
+}
